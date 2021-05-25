2c41f733e091913872bbd46379d8b9eb5a99920e
calumleask <cjleask@hotmail.co.uk>
2021-05-25 20:28:51 +0100

    Updating terser-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,8 +8,6 @@ module.exports = merge(common, {
   devtool: "source-map",
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({
-      extractComments: false
-    })],
+    minimizer: [new TerserPlugin()],
   },
 });
